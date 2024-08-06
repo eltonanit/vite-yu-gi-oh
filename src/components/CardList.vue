33ca8a56ca86ccc4df1304b1f4d08b2c73110d19
eltonanit <elton.ani@gmail.com>
2024-08-06 17:06:51 +0200

    [UPD] Added images card

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -30,16 +30,32 @@ export default {
     <div class="container">
         <div class="row"> 
             <div class="col-20" v-for="card in store.CardList" :key="card.id">
-                {{card.name}}
+        <div class="card"> 
+            <img :src="card.card_images[0].image_url" />
+            <div class="card-body">
+                <h3> {{card.name}} </h3>
+
                 
-            </div> 
+
+
+                
+            </div>  </div> 
+               </div> 
 
         </div> 
     </div>
     
 </template>
 
-<style lang="">
+<style lang="scss" scoped>
+.card {
+    background-color: white;
+    margin: 10px;
+    img {
+        max-width: 100%;
+    }
+
+}
 
 
 </style>
