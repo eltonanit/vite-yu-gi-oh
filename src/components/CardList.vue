5a1d613eabbca58fd3081b80eb379039fffe0259
eltonanit <elton.ani@gmail.com>
2024-08-06 18:10:43 +0200

    [UPD]Fix scss

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,7 +1,11 @@
 <script>
 import axios from 'axios';
 import { store } from '../store.js';
+import DetailCard from './DetailCard.vue';
 export default {
+    components:{
+        DetailCard
+    },
 
     data () {
         return {
@@ -29,18 +33,9 @@ export default {
 <template lang="">
     <div class="container">
         <div class="row"> 
-            <div class="col-20" v-for="card in store.CardList" :key="card.id">
-        <div class="card"> 
-            <img :src="card.card_images[0].image_url" />
-            <div class="card-body">
-                <h3> {{card.name}} </h3>
 
-                
-
-
-                
-            </div>  </div> 
-               </div> 
+            <DetailCard v-for="card in store.CardList"  :key="card.id" :card="card"  />
+           
 
         </div> 
     </div>
@@ -48,14 +43,7 @@ export default {
 </template>
 
 <style lang="scss" scoped>
-.card {
-    background-color: white;
-    margin: 10px;
-    img {
-        max-width: 100%;
-    }
 
-}
 
 
 </style>
---
+++ src/components/DetailCard.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+    props: {
+        card: Object
+    }
+}
+</script>
+
+<template lang="">
+    <div class="col-20" >
+        <div class="card"> 
+            <img :src="card.card_images[0].image_url" />
+            <div class="card-body">
+                <h3> {{card.name}} </h3>
+
+                
+
+
+                
+            </div>  </div> 
+               </div> 
+</template>
+
+<style lang="scss" scoped>
+
+.card {
+    background-color: white;
+    margin: 10px;
+    padding: 10px;
+    img {
+        max-width: 100%;
+    }
+
+} 
+    
+</style>
